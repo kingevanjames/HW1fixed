7f86c22633752ac2e81cb2dc688256f369fed90f
Evan Latham <evan.latham@kellogg.northwestern.edu>
2025-01-29 22:58:10 +0000

    rewrote previous code up to SELECT STATEMENTS from https://github.com/kingevanjames/hw1 -
    had continual errors pop up but couldnot find them, then ran into sql launching errors from trying to restart with a new terminal...so I decided to re-clone the original HQ repository and rewrite the code from scratch.
    
    initial errors turned out to be commas / semicolons syntax, but I could not find them so I just rewrote it because I thought my code was right.
    
    then completed SELECT code.

--- kmdb.sql
+++ kmdb.sql
@@ -113,12 +113,96 @@
 -- Drop existing tables, so you'll start fresh each time this script is run.
 -- TODO!
 
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS studios;
+DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS characters;
+
 -- Create new tables, according to your domain model
 -- TODO!
+
+
+CREATE TABLE studios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+);
+
+CREATE TABLE movies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    year INTEGER,
+    mpaa_rating TEXT,
+    studio_id INTEGER
+);
+
+CREATE TABLE actors (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    actor_name TEXT
+);
+
+CREATE TABLE characters (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    character_name TEXT,
+    movie_id INTEGER,
+    actor_id INTEGER
+);
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
 -- TODO!
+
+INSERT INTO studios (
+    name
+) VALUES (
+    'Warner Bros.'
+);
+
+INSERT INTO movies (
+    title,
+    year,
+    mpaa_rating,
+    studio_id
+) VALUES
+    ('Batman Begins', 2005, 'PG-13', 1),
+    ('The Dark Knight', 2008, 'PG-13', 1),
+    ('the Dark Knight Rises', 2012, 'PG-13', 1);
+
+
+INSERT INTO actors (
+    actor_name
+) VALUES
+    ('Christian Bale'),
+    ('Michael Caine'),
+    ('Liam Neeson'),
+    ('Katie Holmes'),
+    ('Gary Oldman'),
+    ('Heath Ledger'),
+    ('Aaron Eckhart'),
+    ('Maggie Gyllenhaal'),
+    ('Tom Hardy'),
+    ('Joseph Gordon-Levitt'),
+    ('Anne Hathaway');
+
+INSERT INTO characters (
+    character_name,
+    movie_id,
+    actor_id
+) VALUES 
+    ('Bruce Wayne',1,1),
+    ('Alfred', 1, 2),
+    ("Ra's Al Ghul", 1, 3),
+    ('Rachel Dawes', 1, 4),
+    ('Commissioner Gordon', 1, 5),
+    ('Bruce Wayne', 2, 1),
+    ('Joker', 2, 6),
+    ('Harvey Dent', 2, 7),
+    ("Alfred", 2, 2),
+    ('Rachel Dawes', 2, 8),
+    ('Bruce Wayne', 3, 1),
+    ('Commissioner Gordon', 3, 5),
+    ('Bane', 3, 9),
+    ('John Blake', 3, 10),
+    ('Selina Kyle', 3, 11);
 
 -- Prints a header for the movies output
 .print "Movies"
@@ -127,6 +211,11 @@
 
 -- The SQL statement for the movies output
 -- TODO!
+
+SELECT movies.title, movies.year, movies.mpaa_rating, studios.name
+FROM movies
+INNER JOIN studios on movies.studio_id = studios.id;
+
 
 -- Prints a header for the cast output
 .print ""
@@ -137,3 +226,9 @@
 
 -- The SQL statement for the cast output
 -- TODO!
+
+SELECT movies.title, actors.actor_name, characters.character_name
+FROM movies
+INNER JOIN characters ON characters.movie_id = movies.id
+INNER JOIN actors ON actors.id = characters.actor_id
+ORDER BY movies.title
